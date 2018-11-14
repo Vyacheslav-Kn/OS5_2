310b0b38405cbba0a2b0f3d8658a019735feba8a
Vyacheslav Kn <vkniga1999@gmail.com>
2018-11-15 01:02:13 +0300

    Delete events, modified pointerd -> sync works && need to fix writing to files

--- Lab_2_OS/SmartPointer.h
+++ Lab_2_OS/SmartPointer.h
@@ -1,12 +1,4 @@
-#pragma once
-
-#ifndef SMART_POINTER_HEADER
-#define SMART_POINTER_HEADER
-
-#include <cstddef>
-#include <mutex>
-
-using namespace std;
+#include <Windows.h>
 
 namespace SmartPointer
 {
@@ -18,37 +10,35 @@ namespace SmartPointer
 	{
 		private:
 		ref_count_data ref_counts;
-		mutable mutex ref_count_mutex;
 
 		public:
+		HANDLE mutex;
 		reference_count()
 		{
 			ref_counts.use_count = 1;
+			mutex = CreateMutex(NULL, FALSE, NULL);
 		}
 
 		void* get_shared_ref(void* pdata)
 		{
-			lock_guard<mutex> lock(ref_count_mutex);
+			WaitForSingleObject(mutex, INFINITE);
 
 			if (ref_counts.use_count){
 				ref_counts.use_count++;
+				ReleaseMutex(mutex);
 				return pdata;
 			}
 			else{
+				ReleaseMutex(mutex);
 				return NULL;
 			}
 		}
 
 		ref_count_data release_shared_ref(){
-			lock_guard<mutex> lock(ref_count_mutex);
-			ref_counts.use_count--;
+			WaitForSingleObject(mutex, INFINITE);
+			//ref_counts.use_count--;
+			ReleaseMutex(mutex);
 			return ref_counts;
-		}
-
-		unsigned int get_use_count() const
-		{
-			lock_guard<mutex> lock(ref_count_mutex);
-			return ref_counts.use_count;
 		}
 	};
 
@@ -56,19 +46,18 @@ namespace SmartPointer
 	class SafeSmartPointer
 	{
 	private:
-		T* pdata;            // pointer
-		reference_count* rc; // Reference count
+		T* pdata;            
 
 	public:
-		SafeSmartPointer() : pdata(NULL), rc(NULL) {} //default constructor
+		SafeSmartPointer() : pdata(NULL), rc(NULL) {} 
+		reference_count* rc; 
 
 		SafeSmartPointer(T* pvalue) : pdata(pvalue), rc(NULL){
 			if (NULL != pdata){
 				rc = new reference_count();
 			}
 		}
-
-		//Copy constructor
+		
 		SafeSmartPointer(const SafeSmartPointer<T>& sp) : pdata(NULL), rc(sp.rc){
 			if (NULL != rc){
 				pdata = static_cast<T*>(rc->get_shared_ref(sp.pdata));
@@ -83,10 +72,6 @@ namespace SmartPointer
 		{
 			if (NULL != rc){
 				ref_count_data updated_counts = rc->release_shared_ref();
-
-				if (0 == updated_counts.use_count){
-					delete pdata;
-				}
 			}
 		}
 
@@ -94,11 +79,9 @@ namespace SmartPointer
 			return *pdata;
 		}
 
-		T* operator-> () const{
+		T* operator-> () const{			
 			return pdata;
 		}
 		
 	};
 }
-
-#endif
