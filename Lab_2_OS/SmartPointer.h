f525ed3311e9868649818ee81fc6f819fda3957c
Vyacheslav Kn <vkniga1999@gmail.com>
2018-11-17 19:17:31 +0300

    changed pointer, fixed main file

--- Lab_2_OS/SmartPointer.h
+++ Lab_2_OS/SmartPointer.h
@@ -2,94 +2,74 @@
 
 namespace SmartPointer
 {
-	struct ref_count_data{
-		unsigned int use_count;
-	};
-
-	class reference_count
+	template <typename T>
+	class SafeSmartPointer
 	{
-		private:
-		ref_count_data ref_counts;
-
-		public:
+	private:
+		T* data;            
+		int numberOfReferences;
 		HANDLE mutex;
-		reference_count()
-		{
-			ref_counts.use_count = 1;
-			mutex = CreateMutex(NULL, FALSE, NULL);
-		}
 
-		void* get_shared_ref(void* pdata)
+		T* getPointer(T* data)
 		{
 			WaitForSingleObject(mutex, INFINITE);
 
-			if (ref_counts.use_count){
-				ref_counts.use_count++;
+			if (numberOfReferences == 0) {
+				numberOfReferences++;
 				ReleaseMutex(mutex);
-				return pdata;
+				return data;
 			}
-			else{
+			else {
 				ReleaseMutex(mutex);
 				return NULL;
 			}
 		}
 
-		ref_count_data release_shared_ref(){
-			//WaitForSingleObject(mutex, INFINITE);
-			//ref_counts.use_count--;
-			//ReleaseMutex(mutex);
-			return ref_counts;
+		void refreshMutexToSignalState() {
+			CloseHandle(mutex);
+			mutex = CreateMutex(NULL, TRUE, NULL);
 		}
-	};
-
-	template <typename T>
-	class SafeSmartPointer
-	{
-	private:
-		T* pdata;            
 
 	public:
-		SafeSmartPointer() : pdata(NULL), rc(NULL) {} 
-		reference_count* rc; 
+		SafeSmartPointer() : data(NULL), numberOfReferences(0), mutex(NULL) {} 
 
-		SafeSmartPointer(T* pvalue) : pdata(pvalue){
-			if (NULL != pdata){
-				rc = new reference_count();
-			}
-		}
-		
-		/*SafeSmartPointer(const SafeSmartPointer<T>& sp) : pdata(NULL), rc(sp.rc){
-			if (NULL != rc){
-				pdata = static_cast<T*>(rc->get_shared_ref(sp.pdata));
-
-				if (NULL == pdata){
-					rc = NULL;
-				}
-			}
-		}*/
-
-		~SafeSmartPointer()
-		{
-			if (NULL != rc){
-				//CloseHandle(rc->mutex);
-				//ref_count_data updated_counts = rc->release_shared_ref();
-				//rc->~reference_count();
-				//delete rc;
-				//rc = NULL;
-				//delete pdata;
+		SafeSmartPointer(T* value){
+			numberOfReferences = 0;
+			mutex = NULL;
+			if (value != NULL){
+				mutex = CreateMutex(NULL, TRUE, NULL);
+				data = getPointer(value);
 			}
 		}
 
-		T& operator* () const{
-			WaitForSingleObject(rc->mutex, INFINITE);
-			ReleaseMutex(rc->mutex);
-			return *pdata;
+		T& operator* (){
+			if (numberOfReferences == 1) {
+				refreshMutexToSignalState();
+				WaitForSingleObject(mutex, INFINITE);
+				ReleaseMutex(mutex);
+				return *data;
+			}
+
+			ReleaseMutex(mutex);
+			return NULL;
 		}
 
-		T* operator-> () const{		
-			WaitForSingleObject(rc->mutex, INFINITE);
-			ReleaseMutex(rc->mutex);
-			return pdata;
+		T* operator-> (){
+			if (numberOfReferences == 1) {
+				refreshMutexToSignalState();
+				WaitForSingleObject(mutex, INFINITE);
+				ReleaseMutex(mutex);
+				return data;
+			}
+
+			ReleaseMutex(mutex);
+			return NULL;
+		}
+
+		~SafeSmartPointer() {
+			delete data;
+			ReleaseMutex(mutex);
+			CloseHandle(mutex);
 		}
 		
 	};
